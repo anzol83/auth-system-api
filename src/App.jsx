c54ae46cdb124077415c4e8cfa222d64bea9aa6f
Angel Neupane <angelneupane83@gmail.com>
2025-03-18 20:45:51 +1100

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import AuthPage from './pages/Auth/AuthPage'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import VerifyUserPage from './pages/Auth/VerifyUserPage';
+import BooksPage from './pages/Student/BooksPage';
+import StudentPrivateRoute from './components/PrivateStudentRoute';
 
 function App() {
   return (
@@ -8,7 +13,13 @@ function App() {
       <Routes>
         {/* Auth Routes - Public */}
         <Route path='/' element={<AuthPage />} />
+        <Route path='/verify-user' element={<VerifyUserPage />} />
+
+        {/* Private Route - Student Route */}
+        <Route path='/books' element={<StudentPrivateRoute><BooksPage /></StudentPrivateRoute>} />
       </Routes>
+
+      <ToastContainer />
     </>
   )
 }
